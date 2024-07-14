03564f232f2fce3bab61dcf2f13281e7b696d9e4
Antonio <contriantoni@gmail.com>
2024-07-14 21:46:17 +0200

    Ma vuoi vedere...

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import '@fortawesome/fontawesome-free/css/all.css';
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="sitoPolle/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="home" element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
+        <Route path="sitoPolle/home" element={<HomePage />} />
+        <Route path="sitoPolle/about" element={<AboutPage />} />
       </Route>
     </Routes>
   );
