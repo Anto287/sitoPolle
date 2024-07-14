64ef2899614483feff8e755e026ce3357c418fe6
Antonio <contriantoni@gmail.com>
2024-07-14 10:08:09 +0200

    altro boh

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: 'bundle.js',
-      publicPath: isProduction ? '/sitoPolle/' : '/'
+      publicPath: '/'
     },
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     mode: isProduction ? 'production' : 'development',
