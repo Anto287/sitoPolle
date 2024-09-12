1c064128db2aef9c6c39ad13d57f6a058c694f58
Antonio <contriantoni@gmail.com>
2024-09-12 22:32:59 +0200

    start to add animation on change page and fix vulnerability

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Topbar from '@components/Topbar';
 import Menu from '@components/Menu';
 import { useMyData } from '@components/ScrollData';
+import { gsap } from 'gsap';
 
-const Layout = ({ showTopbar  }) => {
+const Layout = ({ showTopbar }) => {
   const { data, setData } = useMyData();
 
   const [showTopBarScrolling, setShowTopBarScrolling] = useState(true);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const lastScrollTop = useRef(0);
   const scrollContainerRef = useRef(null);
+  const pageRef = useRef(null);
+  const location = useLocation();
 
   const handleScroll = () => {
     const scrollTop = scrollContainerRef.current.scrollTop;
@@ -29,26 +32,46 @@ const Layout = ({ showTopbar  }) => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const animatePageTransition = () => {
+    const tl = gsap.timeline();
+
+    tl.to(pageRef.current, {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: () => {
+        gsap.to(pageRef.current, { opacity: 1, duration: 0.5 });
+      }
+    });
+  };
+
   useEffect(() => {
     const scrollContainer = scrollContainerRef.current;
 
     if (scrollContainer) {
-      scrollContainer.addEventListener("scroll", handleScroll);
+      scrollContainer.addEventListener('scroll', handleScroll);
     }
 
     return () => {
       if (scrollContainer) {
-        scrollContainer.removeEventListener("scroll", handleScroll);
+        scrollContainer.removeEventListener('scroll', handleScroll);
       }
     };
   }, []);
 
+  useEffect(() => {
+    animatePageTransition();
+  }, [location]);
+
   return (
     <>
       <div ref={scrollContainerRef} className='container-web'>
-        {showTopbar  && <Topbar showTopBarScrolling={showTopBarScrolling} toggleMenu={toggleMenu}  />}
-        <Menu isMenuOpen={isMenuOpen}/>
-        <Outlet />
+        {showTopbar && (
+          <Topbar showTopBarScrolling={showTopBarScrolling} toggleMenu={toggleMenu} />
+        )}
+        <Menu isMenuOpen={isMenuOpen} />
+        <div ref={pageRef} style={{ opacity: 1 }}> 
+          <Outlet />
+        </div>
       </div>
     </>
   );
