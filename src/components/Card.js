39fcbd96db277dbbcd13d4437a7bc135462dcae6
Antonio <contriantoni@gmail.com>
2024-10-13 19:20:22 +0200

    Modify home description

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import ImgLoader from '@components/ImgLoader';
 import '@styles/Card.css';
 
-const Card = ({imgCard, altImgCard, titleCard, descriptionCard, styleImgLoader, containerClassImgLoader, styleImg}) => {
+const Card = ({ imgCard, altImgCard, titleCard, descriptionCard, styleImgLoader, containerClassImgLoader, styleImg }) => {
     const { t } = useTranslation();
+    const [ripples, setRipples] = useState([]);
+
+    const handleClick = (e) => {
+        const button = e.currentTarget;
+        const rect = button.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const newRipple = { x, y, id: Date.now() };
+        setRipples((prevRipples) => [...prevRipples, newRipple]);
+
+        setTimeout(() => {
+            setRipples((prevRipples) => prevRipples.filter(r => r.id !== newRipple.id));
+        }, 1000);
+    };
 
     return (
-        <div>
-            <div className="card">
-                <ImgLoader 
-                    src={imgCard} 
-                    style={styleImgLoader}
-                    containerClass={containerClassImgLoader}
-                    styleImg={styleImg}
-                    alt={t(altImgCard)}
-                />  
-                <div className="card-content">
+        <div className="card">
+            <ImgLoader 
+                src={imgCard} 
+                style={styleImgLoader}
+                containerClass={containerClassImgLoader}
+                styleImg={styleImg}
+                alt={t(altImgCard)}
+            />  
+            <div className="card-content">
                 <h2 className="card-title">{t(titleCard)}</h2>
                 <p className="card-description">{t(descriptionCard)}</p>
-                <div className="card-action">
-                    <span>{t('LEARN_MORE')}</span>
-                    <i className="fa-solid fa-share-from-square fa-bounce"></i>
-                </div>
+                <div className='card-container-action'>
+                    <div className="card-action" onClick={handleClick}>
+                        {ripples.map(ripple => (
+                            <span 
+                                key={ripple.id} 
+                                className="ripple" 
+                                style={{ left: ripple.x, top: ripple.y }}
+                            />
+                        ))}
+                        <span>{t('LEARN_MORE')}</span>
+                        <i className="fa-solid fa-share-from-square"></i>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/DescriptionHome.js
+++ src/components/DescriptionHome.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import React, { useEffect, useRef  } from 'react';
 import { useTranslation } from 'react-i18next';
 import { UseResponsiveJSX } from '@components/UseResponsiveJSX';
 import { useMyData } from '@components/ScrollData';
+import { gsap } from 'gsap';
 import Card from '@components/Card';
 import '@styles/DescriptionHome.css';
 
@@ -15,8 +15,9 @@ const DescriptionHome = () => {
   const { t } = useTranslation();
   const { data } = useMyData();
   const breakpoint = UseResponsiveJSX([600, 1200, 2000]); 
-  const controls = useAnimation();
-  const controlsCell = useAnimation();
+  const containerRef = useRef(null);
+  const cardsRef = useRef([]);
+
   const listStructure = [
     {
       img: la_tana,
@@ -42,45 +43,80 @@ const DescriptionHome = () => {
 
   useEffect(() => {
     const scrollPosition = data;
-    const triggerPoint = 200; 
-
-    if (scrollPosition > triggerPoint) {
-      controls.start({ opacity: 1, y: 0 });
+    const triggerPoint = 200;
+  
+    // Animazione per il titolo
+    if (scrollPosition > 150) {
+      gsap.to(containerRef.current, {
+        opacity: 1,
+        y: 0,
+        duration: 1.2,
+        ease: 'power4.out',
+        overwrite: 'auto',
+      });
     } else {
-      controls.start({ opacity: 0, y: 50 }); 
+      gsap.to(containerRef.current, {
+        opacity: 0,
+        y: 100,
+        duration: 1.2,
+        ease: 'power4.out',
+        overwrite: 'auto',
+      });
     }
+  
+    // Animazione per le card
+    listStructure.forEach((_, index) => {
+      const cardTriggerPoint = triggerPoint + index * 475;
+      if (scrollPosition > cardTriggerPoint) {
+        gsap.to(cardsRef.current[index], {
+          opacity: 1,
+          y: 0,
+          scale: 1,
+          duration: 1.2,
+          ease: 'power4.out',
+          overwrite: 'auto',
+        });
+      } else {
+        gsap.to(cardsRef.current[index], {
+          opacity: 0,
+          y: 70,
+          scale: 0.9,
+          duration: 1.2,
+          ease: 'power4.out',
+          overwrite: 'auto',
+        });
+      }
+    });
 
-    if(scrollPosition > (triggerPoint + 70)){
-      controlsCell.start({ opacity: 1, y: 0 });
-    }else{
-      controlsCell.start({ opacity: 0, y: 70 });
-    }
+    return () => {
+      gsap.killTweensOf(containerRef.current);
+      cardsRef.current.forEach(card => gsap.killTweensOf(card));
+    };
   }, [data]);
 
   return (
     <div>
       {breakpoint === 0 && 
         <div className='content-where-are-mobile'>
-          <div className='container-paragraph-mobile'>
-            <motion.div
-              initial={{ opacity: 0, y: 50 }}
-              animate={controls}
-              transition={{ duration: 0.5 }} 
-            >
-              <h1>{t('OUR_STRUCTURE')}</h1>
-            </motion.div>
+          <div className='container-paragraph-mobile' ref={containerRef} style={{ opacity: 0 }}>
+            <h1>{t('OUR_STRUCTURE')}</h1>
           </div>
           <div className='container-cell-img-mobile'>
             {(listStructure || []).map((el, index) => (
-              <Card 
-                imgCard={el?.img} 
-                altImgCard={el?.title}
-                titleCard={el?.title}
-                descriptionCard={el?.description}
-                styleImgLoader={{minWidth: '40px', width: '100%', maxWidth: '100%', height: 'auto', maxHeight: '225px'}}
-                containerClassImgLoader="zoom-effect" 
-                styleImg={{width: '100%', height: 'auto'}}
-              ></Card>
+              <div 
+                key={index}
+                ref={(el) => (cardsRef.current[index] = el)} 
+                style={{ opacity: 0 }}
+              >
+                <Card
+                  imgCard={el?.img} 
+                  altImgCard={el?.title}
+                  titleCard={el?.title}
+                  descriptionCard={el?.description}
+                  styleImgLoader={{minWidth: '40px', width: '100%', maxWidth: '100%', height: 'auto', maxHeight: '225px'}}
+                  styleImg={{width: '100%', height: 'auto'}}
+                />
+              </div>              
             ))}
           </div>
         </div>
